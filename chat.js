ba85b6214640e19252967240a73ecffc887e156a
Eelis Ewert <ewert99@gmail.com>
2018-05-06 12:04:21 +0200

    Test

---
+++ chat.js
@@ -0,0 +1,216 @@
+var $messages = $('.messages-content'),
+    d, h, m,
+    i = 0;
+var result;
+//********************** Socket.io **************************/
+
+ 
+  var connectionOptions =  {
+            "force new connection" : true,
+            "reconnectionAttempts": "Infinity", //avoid having user reconnect manually in order to prevent dead clients after a server restart
+            "timeout" : 10000, //before connect_error and connect_timeout are emitted.
+            "transports" : ["websocket"]
+ };
+// var socket = io("http://213.89.12.1:3000", connectionOptions); 
+ var socket = io("http://localhost:3000", connectionOptions); 
+            if(socket !== undefined){
+                console.log('Connected to socket...')
+ }	 
+	
+    socket.on('eeTalk node-client',function(data){	
+	   console.info(data.message);
+	   
+	  $('<div class="message new"> <figure class="avatar"><img src="http://askavenue.com/img/17.jpg" /></figure>' + data.message + '</div>').appendTo($('.messages-content')).addClass('new');
+      setDate();
+	});		
+	
+function eeTalkSendMessage(msg){
+
+    if(localStorage.getItem("eeTalkUser") != null){		
+		socket.emit('eeTalk client-node', { vendor: "eelis", message: msg, ID: localStorage.getItem("eeTalkUser")});		
+	}else{
+        localStorage.setItem("eeTalkUser", Math.random());
+		socket.emit('eeTalk client-node', { vendor: "eelis", message: msg, ID: localStorage.getItem("eeTalkUser")});		
+	}
+};	
+
+function eeTalkCancelSession(){
+		socket.emit('eeTalk cancelSession', { vendor: "eelis", ID: localStorage.getItem("eeTalkUser")});		
+};	
+	
+
+(function () {
+
+var original = document.title;
+var timeout;
+
+
+
+window.flashTitle = function (newMsg, howManyTimes) {
+    function step() {
+        document.title = (document.title == original) ? newMsg : original;
+
+        if (--howManyTimes > 0) {
+            timeout = setTimeout(step, 1000);
+        };
+    };
+
+    howManyTimes = parseInt(howManyTimes);
+
+    if (isNaN(howManyTimes)) {
+        howManyTimes = 5;
+    };
+
+    cancelFlashTitle(timeout);
+    step();
+};
+
+window.cancelFlashTitle = function () {
+    clearTimeout(timeout);
+    document.title = original;
+};
+
+
+window.onclick=function() { 
+    clearTimeout(timeout);
+    document.title = original;
+}
+}());
+
+
+function updateScrollbar() {
+$('.messages-content').animate({ scrollTop: 9999 }, 'slow');
+}
+
+function setDate(){
+  d = new Date()
+  if (m != d.getMinutes()) {
+    m = d.getMinutes();
+    $('<div class="timestamp">' + d.getHours() + ':' + m + '</div>').appendTo($('.message:last'));
+    $('<div class="checkmark-sent-delivered">&check;</div>').appendTo($('.message:last'));
+    $('<div class="checkmark-read">&check;</div>').appendTo($('.message:last'));
+  }
+}
+
+function insertMessage() {
+  msg = $('.message-input').val();
+  eeTalkSendMessage(msg);
+  if ($.trim(msg) == '') {
+    return false;
+  }
+  $('<div class="message message-personal">' + msg + '</div>').appendTo($('.messages-content')).addClass('new');
+  setDate();
+  $('.message-input').val(null);
+  updateScrollbar();
+
+}
+
+$('.message-submit').click(function() {
+  insertMessage();
+});
+
+$(window).on('keydown', function(e) {
+  if (e.which == 13) {
+    insertMessage();
+    return false;
+  }
+})
+
+
+$('.button').click(function(){
+  $('.menu .items span').toggleClass('active');
+   $('.menu .button').toggleClass('active');
+});
+
+
+$(document ).ready(function() {
+	
+	
+	  if(localStorage.getItem("eeTalkUser") != null){		
+		socket.emit('eeTalk updateSocket', { vendor: "eelis", ID: localStorage.getItem("eeTalkUser")});		
+	}
+	
+	  var minimerad = localStorage.getItem("minimerad");
+	  
+	
+		    $('<div class="message loading new"><figure class="avatar"><img src="http://askavenue.com/img/17.jpg" /></figure><span></span></div>').appendTo($('.messages-content'));
+	
+	   setTimeout(
+  function() 
+      {
+   $('.message.loading').remove();
+   $('<div class="message new"><figure class="avatar"><img src="http://askavenue.com/img/17.jpg" /></figure>' + 'Tja, välkommen' + '</div>').appendTo($('.messages-content')).addClass('new');
+       }, 580);
+	
+		$("#avslutaChatt").mousedown(function(){
+			
+			eeTalkCancelSession();
+			localStorage.removeItem("eeTalkUser");
+			         $(".avenue-messenger").css('opacity', '0');
+		  $(".avenue-messenger").css('height', '230');
+		      $(".avenue-messenger").css('width', '310');
+		   $('.menu .items span').toggleClass('active');
+		    $('.menu .button').toggleClass('active');
+			  localStorage.setItem("minimerad", "ja");
+		})
+	
+	$("#minimera").mousedown(function(){
+         $(".avenue-messenger").css('opacity', '0');
+		  $(".avenue-messenger").css('height', '230');
+		      $(".avenue-messenger").css('width', '310');
+		   $('.menu .items span').toggleClass('active');
+		    $('.menu .button').toggleClass('active');
+			  localStorage.setItem("minimerad", "ja");
+setTimeout(
+  function() 
+      {
+         $(".agent-face-minimized").css('opacity', '1');
+	       $(".agent-face-minimized").css('bottom', '54');
+		    $(".agent-face-minimized").css('transform', 'scale(1)');
+       }, 395);
+ });
+	 
+	 
+	 	$(".circle2").mousedown(function(){
+			
+			setTimeout(
+  function() 
+      {
+		$(".avenue-messenger").css('width', '330');
+         $(".avenue-messenger").css('opacity', '1');
+		  $(".avenue-messenger").css('height', '460');
+ }, 195);
+
+         $(".agent-face-minimized").css('opacity', '0');
+	       $(".agent-face-minimized").css('bottom', '40');
+		    $(".agent-face-minimized").css('transform', 'scale(0)');
+
+setTimeout(
+  function() 
+      {
+		  $(".avenue-messenger").css('height', 'calc(100% - 60px)');
+    }, 660);
+						
+});
+	 
+	 
+
+  var time = localStorage.getItem("expiry");
+  var currentTime;
+  
+var listen = setInterval(function(){  
+    
+  currentTime = Date.now();
+  
+  if(time <= currentTime - 6000){clearInterval(listen);}
+  
+  else{
+  
+  listener();
+
+  }  
+  
+   }, 1000);
+  
+ 
+});
---
+++ goose.js
@@ -0,0 +1,39 @@
+var html = '<section class="avenue-messenger"> <div class="menu"> <div class="items"><span> <a href="#" title="¨Minimera" id="minimera">&mdash;</a><br><!--     <a href="">enter email</a><br><a href="">email transcript</a><br>--> <a href="#" title="Avsluta Chat" id="avslutaChatt">&#10005;</a></span></div><div class="button">...</div></div><div class="chat"><div class="chat-title"><h1>Jessica</h1><h2>Kundtjänst</h2>  <!--  <figure class="avatar">    <img src="http://askavenue.com/img/17.jpg" /></figure>--> </div><div class="messages">  <div class="messages-content"></div> </div> <div class="message-box"><textarea type="text" class="message-input" placeholder="Skriv ditt meddelande..."></textarea><button type="submit" class="message-submit">Skicka</button></div></div>  </div><div class="agent-face" ><div class="half"><img class="agent circle" src="http://askavenue.com/img/17.jpg" alt="Jessica Tino"></div></div></section><div class="agent-face-minimized"><img class="agent circle2" src="http://askavenue.com/img/17.jpg" style=" box-shadow: 1px 4px 20px rgba(22, 20, 19, 0.21);"/></div></div>';
+var js = 'https://rawgit.com/ewert99/Git/master/chat.js'; //Kund variabel 1
+
+ var jquery = document.createElement('script');
+  jquery.setAttribute("src", 'https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js');
+  document.head.appendChild(jquery); 
+
+ var eeTalkSocketIo = document.createElement('script');
+  eeTalkSocketIo.setAttribute("src", 'https://cdnjs.cloudflare.com/ajax/libs/socket.io/2.1.0/socket.io.js');
+  document.head.appendChild(eeTalkSocketIo); 		
+
+ var eeTalkCss = document.createElement('link');
+  eeTalkCss.rel += "stylesheet";  
+  eeTalkCss.setAttribute("href", 'https://rawgit.com/ewert99/Git/master/chat.css'); //Kund variabel 2
+  document.head.appendChild(eeTalkCss); 
+  
+function writeChat(html,js){
+		
+		
+  var chatHtml = document.createElement('div');
+  chatHtml.innerHTML = html.trim();      
+  chatHtml.id += "eE-Talk";  
+
+  var script = document.createElement('script');
+  script.setAttribute("src", js);
+  
+ document.body.appendChild(chatHtml);
+ document.body.appendChild(script); 
+		
+}
+
+
+window.onload = function() {
+
+ writeChat(html,js);
+  
+};
+
+
